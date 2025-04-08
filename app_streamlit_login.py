f40ee01009023d91e4d928a2641919689f35f049
belmone-carteirinha <clariny1@hotmail.com>
2025-04-08 14:56:47 -0300

    Update app_streamlit_login.py

--- app_streamlit_login.py
+++ app_streamlit_login.py
@@ -83,12 +83,12 @@ if not st.session_state.get("autenticado", False):
         usuario = st.text_input("Usuário")
         senha = st.text_input("Senha", type="password")
         if st.button("Entrar"):
-if usuario in st.session_state.usuarios and st.session_state.usuarios[usuario] == senha:
-    st.session_state.autenticado = True
-    st.success("Login realizado com sucesso!")
-    st.stop()
-else: 
-    st.error("Usuário ou senha incorretos")
+            if usuario in st.session_state.usuarios and st.session_state.usuarios[usuario] == senha:
+                st.session_state.autenticado = True
+st.success("Login realizado com sucesso!")
+st.stop()  # Interrompe a execução para reiniciar na próxima interação
+            else:
+                st.error("Usuário ou senha incorretos")
 
     elif menu == "Cadastrar novo usuário":
         novo_usuario = st.text_input("Novo usuário")
