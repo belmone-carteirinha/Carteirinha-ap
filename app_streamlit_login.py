62b25f5bf5d5c8c480327079b36e1945157c1082
belmone-carteirinha <clariny1@hotmail.com>
2025-04-09 21:07:20 -0300

    Update app_streamlit_login.py

--- app_streamlit_login.py
+++ app_streamlit_login.py
@@ -66,7 +66,7 @@ def gerar_carteirinha(nome, curso, matricula, validade, foto):
     c.drawImage(
         caminho_foto,
         6 * mm,
-        altura / 2 - -16 * mm,
+        altura / 35 - -16 * mm,
         width=20 * mm,
         height=23 * mm
     )
